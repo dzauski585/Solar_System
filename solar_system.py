5d4714268e6667f9baeb18afbb86fe22949cffde
Dan Z <dzauski585@gmail.com>
2026-01-25 09:55:44 -0500

    sun in center

--- solar_system.py
+++ solar_system.py
@@ -1,11 +1,15 @@
 import pygame
+import math
  
 # Initialize pygame 
 pygame.init()
  
-WIDTH = 500
-HEIGHT = 500
-screen = pygame.display.set_mode((2000, 1000))
+WIDTH = 2000
+HEIGHT = 1000
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) #Width, height in tuple
  
 pygame.display.set_caption("The Solar System")
  
@@ -16,7 +20,28 @@ while running:
             running = False
 
 
-pygame.display.update()
+    screen.fill(BLACK)
+ 
+    center_x = WIDTH // 2  # What operation finds the middle?
+    center_y = HEIGHT // 2
+    
+    sun_radius = 30
+    mercury_radius = 4
+    venus_radius = 6
+    earth_radius = 6
+    mars_radius = 5
+    jupiter_radius = 15
+    saturn_radius = 12
+    uranus_radius = 8
+    neptune_radius = 8
+    
+    pygame.draw.circle(screen, YELLOW, (center_x, center_y), sun_radius)
+
+
+
+
+
+    pygame.display.update()
  
 
 pygame.quit()
