baea15d84a84dbe0656e5c1c76f586e010a8cc81
Dan Z <dzauski585@gmail.com>
2026-01-25 09:36:19 -0500

    functioning window with close function

--- solar_system.py
+++ solar_system.py
@@ -5,16 +5,18 @@ pygame.init()
  
 WIDTH = 500
 HEIGHT = 500
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((2000, 1000))
  
 pygame.display.set_caption("The Solar System")
  
 running = True
 while running:
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-    # Update the display - what function refreshes the screen?
-        pygame.display.update()
+
+pygame.display.update()
  
-# Clean up - what should you call when done with pygame?
+
 pygame.quit()
