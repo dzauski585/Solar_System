0a76b114a01961c9f196f145bb175c981b35aeca
Dan <75378851+dzauski585@users.noreply.github.com>
2026-02-25 15:40:08 +0000

    planet class, sun, mercury,

--- SOLAR.py
+++ SOLAR.py
@@ -28,7 +28,24 @@ class Planet:
     SCALE = 250 / (1.496e11)  # Pixels per meter (200 pixels = 1 AU)
     TIMESTEP = 86400        # 1 day in seconds
 
-    def init(name, color, radius, ):
+    def __init__(self, name, color, mass, x, y, radius, y_vel=0 ):
+        self.name = name
+        self.color = color
+        self.mass = mass
+        self.x = x
+        self.y = y
+        self. radius = radius
+        self.x_vel = 0
+        self.y_vel = y_vel
+
+sun_body = ("Sun", YELLOW, 1.989e30, 0, 0, 30)
+mercury = Planet("Mercury", BROWN, 3.301e23, 0.387 * Planet.AU, 0, 8, 35020 )
+
+
+planets = [
+    sun_body,
+    mercury, venus, earth, mars, jupiter, saturn, uranus, neptune,
+    ceres, pluto, haumea, makemake, eris,]
 
 
 running = True
@@ -40,6 +57,10 @@ while running:
             running = False
 
     screen.fill(BLACK)
+
+    for planet in planets:
+        planet.draw(screen)
+
 
     pygame.display.update()
 
